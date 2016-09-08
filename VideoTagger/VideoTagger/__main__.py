507985a629fc002260e27b733aae1a4dde2f9dc8
Dan Bright <productions@zaziork.com>
2016-09-09 00:53:15 +0100

    Remove notes

--- VideoTagger/VideoTagger/__main__.py
+++ VideoTagger/VideoTagger/__main__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from .src import run_gui as run
-# note: when running in PyCharm, remove relative "." from src. Add it back to create python package.
+# dev note: when testing in PyCharm, remove relative "." from src. Add it back to create python package.
 
 if __name__ == '__main__':
     run.Main()
