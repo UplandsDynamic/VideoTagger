726ac0c720b02a3a51235837cf32936ce558f7ec
Dan Bright <productions@zaziork.com>
2016-09-10 20:46:48 +0100

    Fix path bug

--- VideoTagger/VideoTagger/__main__.py
+++ VideoTagger/VideoTagger/__main__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from src import run_gui as run
+from .src import run_gui as run
 # dev note: when testing in PyCharm, remove relative "." from src. Add it back to create python package.
 
 if __name__ == '__main__':
